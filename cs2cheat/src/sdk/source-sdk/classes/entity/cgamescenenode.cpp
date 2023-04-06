2ef3593159c6378991a32ebc4a33a45da844a5e3
Marius <53657322+bruhmoment21@users.noreply.github.com>
2023-04-06 21:25:32 +0300

    better setmeshgroupmask sig

--- cs2cheat/src/sdk/source-sdk/classes/entity/cgamescenenode.cpp
+++ cs2cheat/src/sdk/source-sdk/classes/entity/cgamescenenode.cpp
@@ -3,5 +3,6 @@
 #include "../../../memory/memory.hpp"
 
 void CGameSceneNode::SetMeshGroupMask(uint64_t meshGroupMask) {
+    if (!memory::fnSetMeshGroupMask) return;
     return memory::fnSetMeshGroupMask(this, meshGroupMask);
 }
